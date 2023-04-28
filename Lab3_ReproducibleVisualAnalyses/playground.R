cd01336c31464eed46ef9b9b33cb59f9ad94fb18
Nikita Makarevich <nikita_makarevich@yahoo.com>
2023-04-28 21:43:51 +0200

    All 3 visualizations ready. Initial incorporation into report

--- Lab3_ReproducibleVisualAnalyses/playground.R
+++ Lab3_ReproducibleVisualAnalyses/playground.R
@@ -1,6 +1,7 @@
 library(tidyr)
 library(dplyr)
 library(ggplot2)
+library(plotly)
 
 #=================
 
@@ -36,10 +37,20 @@ fig_quant_to_parts = fig_num_quantities %>%
   inner_join(minifigs, by="fig_num")
   
 ggplot(fig_quant_to_parts, aes(x=total_quant, y=num_parts)) +
-  geom_jitter(position=position_jitter(width=0.5, height=0.5), alpha=0.5) +
-  scale_x_sqrt() + 
-  scale_y_sqrt() +
-  theme_bw()
+  geom_jitter(position=position_jitter(width=0.5, height=0.5), 
+              alpha=0.1, 
+              color="#1d91c0") +
+  geom_rect(aes(xmin=0.32, xmax=3.2, ymin=1.3, ymax=13),
+            color="#0c2c84",
+            alpha=0,
+            size=1) +
+  scale_x_log10(n.breaks=10) + 
+  scale_y_log10(n.breaks=10) +
+  labs(title="Sets according to their quantity and parts",
+       x="Total quantity of set",
+       y="Number of parts in set") +
+  theme_bw() +
+  theme(plot.title=element_text(hjust=0.5))
 
 # GOOD, Barplot of number of new sets per year
 #================
@@ -106,17 +117,39 @@ ggplot(color_quants, aes(x=factor(name), y=tot_quant)) +
 # Heatmap stuff
 #================
 
-ggplot(sets, aes(x=year, y=theme_id, fill=num_parts)) +
+get_root_ids = function(theme_ids) {
+  root_ids = rep(0, length(theme_ids))
+  i = 1
+  for (theme_id in theme_ids) {
+    curr_id = theme_id
+    parent_id = themes %>%
+      filter(id == curr_id) %>%
+      .[1, "parent_id"]
+    while (!is.na(parent_id)) {
+      curr_id = parent_id
+      parent_id = themes %>%
+        filter(id == curr_id) %>%
+        .[1, "parent_id"]
+    }
+    root_ids[i] = curr_id
+    i = i + 1
+  }
+  return(root_ids)
+}
+
+themes_mod = themes %>%
+  mutate(root_id=get_root_ids(id))
+
+themes_years = sets %>%
+  inner_join(themes_mod, by=join_by(theme_id == id)) %>%
+  group_by(year, root_id) %>%
+  summarize(count=n()) %>%
+  inner_join(themes, by=join_by(root_id == id))
+
+ggplot(themes_years, aes(x=year, y=name, fill=count)) +
   geom_tile()
 
-x = seq(1, 10)
-y = seq(1, 10)
-temp_data = expand.grid(X=x, Y=y)
-temp_data$Z = runif(100, 0, 5)
-ggplot(temp_data, aes(x=X, y=Y, fill=Z)) +
-  geom_tile()
-
-# Number of available colors for each category
+# GOOD. Number of available colors for each category
 #================
 
 categ_color_cnts = invent_parts %>%
@@ -126,16 +159,32 @@ categ_color_cnts = invent_parts %>%
   inner_join(part_categs, by=join_by(part_cat_id == id)) %>%
   arrange(color_cnt)
 
-ggplot(categ_color_cnts, aes(x=factor(name, levels=name), y=color_cnt)) +
-  geom_bar(stat="identity") +
+splitcat = rep("middle", nrow(categ_color_cnts))
+n_best = 10
+splitcat[1:n_best] = rep(paste("Worst", n_best), n_best)
+splitcat[(66-n_best+1):66] = rep(paste("Best", n_best), n_best)
+categ_color_cnts$splitcat = splitcat
+categ_color_cnts = categ_color_cnts %>%
+  filter(splitcat != "middle")
+
+bar_colors = rep(c("#225ea8", "#41b6c4"), length.out=nrow(categ_color_cnts))
+
+p = ggplot(categ_color_cnts, aes(x=factor(name, levels=name), y=color_cnt)) +
+  geom_bar(stat="identity", fill=bar_colors) +
   coord_flip() +
-  theme_bw()
+  scale_y_continuous(limits=c(0, 120), expand=c(0, 0)) +
+  facet_grid(splitcat ~ ., scales="free") +
+  labs(y="Number of available colors",
+       x="Category",
+       title="Available colors for each category") +
+  theme_minimal()
 
+p
 
-# Scatter plot with all colors and their counts
+ggplotly(p)
+
+# POSSIBLY GOOD. Scatter plot with all colors and their counts
 #================
-
-strtoi(c("12", "34", "56"))
 
 colorsum = function(rgb_colors) {
   res = rep(0, length(rgb_colors))
@@ -165,3 +214,60 @@ ggplot(color_cnts, aes(x=rgb, y=color_cnt)) +
   geom_point(color=color_cnts$rgb, size=3) +
   scale_y_log10() +
   theme_bw()
+
+
+#================
+
+invent_parts_col = invent_parts %>%
+  inner_join(colors, by=join_by(color_id == id)) %>%
+  mutate(color_sum=colorsum(rgb))
+
+ggplot(invent_parts_col, aes(x=color_sum)) +
+  geom_histogram(bins=20)
+
+
+ggplot(sets, aes(x=num_parts)) +
+  geom_histogram() +
+  scale_y_sqrt() +
+  theme_bw()
+
+#================
+
+themes_first_years = sets %>%
+  group_by(theme_id) %>%
+  summarize(first_year=min(year))
+
+themes_quants = invent_sets %>%
+  inner_join(sets, by="set_num") %>%
+  group_by(theme_id) %>%
+  summarize(tot_quant=sum(quantity)) %>%
+  top_n(10, tot_quant)
+
+theme_avg_parts = sets %>%
+  group_by(theme_id) %>%
+  summarize(avg_parts=mean(num_parts))
+
+thm_quants_fyears = themes_first_years %>%
+  inner_join(themes_quants, by="theme_id") %>%
+  inner_join(theme_avg_parts, by="theme_id") %>%
+  inner_join(themes, by=join_by(theme_id == id)) %>%
+  arrange(desc(avg_parts))
+
+colors = c("#081d58", "#253494", "#225ea8", "#1d91c0", "#41b6c4",
+           "#7fcdbb", "#c7e9b4", "#edf8b1", "#ffffd9", "#ffffff")
+vjusts = c(3.6, 4, 3.3, 3, 3, 2, -2.5, 1.7, 1, -2)
+hjusts= c(-0.2, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5)
+ggplot(thm_quants_fyears, aes(x=first_year, y=tot_quant,label=name)) +
+  geom_point(aes(size=avg_parts), fill=colors, color="black", shape=21, alpha=0.7) +
+  scale_size(name="Size", range=c(5, 35)) +
+  geom_point(size=1.5, color="black") +
+  geom_text(size=4, color="black", hjust=hjusts, vjust=-vjusts) +
+  scale_x_continuous(limits=c(1980, 2025)) +
+  scale_y_continuous(expand=c(0.1, 0.1)) +
+  theme_minimal() +
+  theme(legend.position="none", plot.title=element_text(size=18, hjust=0.5)) +
+  labs(x="Year of First Introduction",
+       y="Total Quantity in Inventories",
+       title="Top 10 Themes of Sets",
+       subtitle="(size indicates average number of parts in set)")
+  
